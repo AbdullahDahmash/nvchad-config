501432e5f74967b33fe34f0970e0083172f94913
Abdullah Nasser Dahmash <Abdullah.Dahmash@MacBook-Pro-Sysadmin-zain.local>
2025-08-11 14:41:57 +0300

    fixed install path issue

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,7 +19,7 @@ return {
     config = function ()
       local mason_registry = require('mason-registry')
       local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local extension_path = vim.fs.joinpath(codelldb.install_path) .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
       local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
 	-- If you are on Linux, replace the line above with the line below:
